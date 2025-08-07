8a804484e72cca04c29c702997f2027613e01611
Sai Prasad <2410040072@klh.edu.in>
2025-08-07 15:28:07 +0530

    Remove all files from main

--- Untitled1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	int n=10;
-  do
-	{
-	printf("Hi\n");
-    }while(n>5);
-}
--- file.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	FILE* ptr=fopen("sai.txt","w");
-	char s[]="sai";
-	fputs(s,ptr);
-	fclose(ptr);
-
-}
